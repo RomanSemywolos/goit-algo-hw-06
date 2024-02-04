3ff1dc7f86def05b7b504d052ca2e6d20061e507
Roman Semywolos <Roman.Semywolos@gmail.com>
2024-02-04 08:07:57 +0200

    first commit

---
+++ dfs_bfs_tree.py
@@ -0,0 +1,41 @@
+from graph import subway, show_graph
+import networkx as nx
+
+
+def dfs_paths(graph, start, end, path=[]):
+    path = path + [start]
+    if start == end:
+        return [path]
+    paths = []
+    for neighbor in graph.neighbors(start):
+        if neighbor not in path:
+            new_paths = dfs_paths(graph, neighbor, end, path)
+            for p in new_paths:
+                paths.append(p)
+    return paths
+
+
+def bfs_paths(graph, start, end):
+    queue = [(start, [start])]
+    paths = []
+    while queue:
+        current, path = queue.pop(0)
+        for neighbor in graph.neighbors(current):
+            if neighbor not in path:
+                new_path = path + [neighbor]
+                queue.append((neighbor, new_path))
+                if neighbor == end:
+                    paths.append(new_path)
+    return paths
+
+start = "Heroiv Dnipra"
+end = "Hydropark"
+
+dfs_tree = dfs_paths(subway, start, end)
+bfs_tree = bfs_paths(subway, start, end)
+
+dfs_tree = nx.dfs_tree(subway, source="Heroiv Dnipra")
+bfs_tree = nx.bfs_tree(subway, source="Heroiv Dnipra")
+
+show_graph(dfs_tree, "DFS Tree")
+show_graph(bfs_tree, "BFS Tree")
---
+++ graph.py
@@ -0,0 +1,94 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+
+subway = nx.Graph()
+subway.add_edges_from(
+    [   ("Heroiv Dnipra", "Minska", {"weight": 3}),
+        ("Minska", "Obolon", {"weight": 3}),
+        ("Obolon", "Pochaina", {"weight": 3}),
+        ("Pochaina", "Ploshcha Tarasa Shevchenka", {"weight": 3}),
+        ("Ploshcha Tarasa Shevchenka", "Kontraktova Ploshcha", {"weight": 3}),
+        ("Kontraktova Ploshcha", "Poshtova Ploshcha", {"weight": 3}),
+        ("Poshtova Ploshcha", "Maidan Nezalezhnosti", {"weight": 3}),
+        ("Maidan Nezalezhnosti", "Ploshcha Ukrainskykh Heroiv", {"weight": 3}),
+        ("Ploshcha Ukrainskykh Heroiv", "Olimpiyska", {"weight": 3}),
+        ("Olimpiyska", "Palats Ukraina", {"weight": 3}),
+        ("Palats Ukraina", "Lybidska", {"weight": 3}),
+        ("Lybidska", "Demiivska", {"weight": 3}),
+        ("Demiivska", "Holosiivska", {"weight": 3}),
+        ("Holosiivska", "Vasylkivska", {"weight": 3}),
+        ("Vasylkivska", "Vystavkovyi Tsentr", {"weight": 3}),
+        ("Vystavkovyi Tsentr", "Ipodrom", {"weight": 3}),
+        ("Ipodrom", "Teremky", {"weight": 3}),
+
+        ("Lisova", "Chernihivska", {"weight": 3}),
+        ("Chernihivska", "Darnytsia", {"weight": 3}),
+        ("Darnytsia", "Livoberezhna", {"weight": 3}),
+        ("Livoberezhna", "Hydropark", {"weight": 3}),
+        ("Hydropark", "Dnipro", {"weight": 3}),
+        ("Dnipro", "Arsenalna", {"weight": 3}),
+        ("Arsenalna", "Khreshchatyk", {"weight": 4}),
+        ("Khreshchatyk", "Teatralna", {"weight": 3}),
+        ("Teatralna", "Universytet", {"weight": 3}),
+        ("Universytet", "Vokzalna", {"weight": 3}),
+        ("Vokzalna", "Politekhnichnyi Instytut", {"weight": 3}),
+        ("Politekhnichnyi Instytut", "Shuliavska", {"weight": 3}),
+        ("Shuliavska", "Beresteiska", {"weight": 3}),
+        ("Beresteiska", "Nyvky", {"weight": 3}),
+        ("Nyvky", "Sviatoshyn", {"weight": 3}),
+        ("Sviatoshyn", "Zhytomyrska", {"weight": 3}),
+        ("Zhytomyrska", "Akademmistechko", {"weight": 3}),
+
+        ("Syrets", "Dorohozhychi", {"weight": 3}),
+        ("Dorohozhychi", "Lukianivska", {"weight": 3}),
+        ("Lukianivska", "Zoloti Vorota", {"weight": 4}),
+        ("Zoloti Vorota", "Palats Sportu", {"weight": 3}),
+        ("Palats Sportu", "Klovska", {"weight": 3}),
+        ("Klovska", "Pecherska", {"weight": 3}),
+        ("Pecherska", "Zvirynetska", {"weight": 3}),
+        ("Zvirynetska", "Vydubychi", {"weight": 3}),
+        ("Vydubychi", "Slavutych", {"weight": 4}),
+        ("Slavutych", "Osokorky", {"weight": 3}),
+        ("Osokorky", "Pozniaky", {"weight": 3}),
+        ("Pozniaky", "Kharkivska", {"weight": 3}),
+        ("Kharkivska", "Vyrlytsia", {"weight": 3}),
+        ("Vyrlytsia", "Boryspilska", {"weight": 3}),
+        ("Boryspilska", "Chervonyi Khutir", {"weight": 3}),
+
+        ("Maidan Nezalezhnosti", "Khreshchatyk", {"weight": 5}),
+        ("Teatralna", "Zoloti Vorota", {"weight": 5}),
+        ("Palats Sportu", "Ploshcha Ukrainskykh Heroiv", {"weight": 5}),
+    ]
+)
+
+pos = nx.circular_layout(subway)
+
+def show_graph(graph, title="Graph"):
+    pos = nx.circular_layout(graph)
+    nx.draw(
+        graph,
+        pos,
+        with_labels=True,
+        font_size=7,
+        node_size=300,
+        node_color="skyblue",
+        font_color="black",
+        font_weight="bold",
+        arrowsize=10,
+    )
+    plt.title(title)
+    plt.show()
+
+show_graph(subway, "Kyiv metro")
+
+num_nodes = subway.number_of_nodes()
+num_edges = subway.number_of_edges()
+
+print(f"Nodes: {num_nodes}")
+print(f"Edges: {num_edges}")
+
+degree_dict = dict(subway.degree())
+print("\nDegrees:")
+for station, degree in degree_dict.items():
+    print(f"{station}: {degree}")
